8790cdd2501141bea2b468310cfb8a8212bf95ba
Bart Devriendt <bart.devriendt@gmail.com>
2024-02-10 10:23:10 +0100

    Add some tests

---
+++ test/GradsSharp.UnitTests/DrawingTests/ContourTests.cs
@@ -0,0 +1,74 @@
+﻿using System.Drawing;
+using System.Reflection;
+using GradsSharp.Data;
+using GradsSharp.Data.GridFunctions;
+using GradsSharp.DataReader.GFS;
+using GradsSharp.DrawingEngine.Cairo;
+using GradsSharp.Models;
+
+namespace GradsSharp.UnitTests;
+
+public class ContourTests
+{
+    private GradsEngine engine;
+    private IGriddedDataReader reader;
+    
+    [SetUp]
+    public void Setup()
+    {
+        engine = new GradsEngine();
+        engine.RegisterDrawingEngine(new CairoDrawingEngine(engine));
+        engine.InitEngine();
+
+        reader = new GFSDataReader();
+    }
+
+    [Test]
+    public void ContourTestTemperature2m()
+    {
+        engine.GradsCommandInterface.SetGrads(OnOffSetting.Off);
+        engine.GradsCommandInterface.Open("Data//gfs.t00z.pgrb2.0p25.f001", reader);
+        engine.GradsCommandInterface.SetMPVals(OnOffSetting.On, -2.9,12,47,56);
+        engine.GradsCommandInterface.SetMapResolution(MapResolution.HighResolution);
+        engine.GradsCommandInterface.SetGridOptions(GridOption.On);
+        engine.GradsCommandInterface.SetLat(47,56);
+        engine.GradsCommandInterface.SetLon(-2.9,12);
+        engine.GradsCommandInterface.SetT(1);
+        engine.GradsCommandInterface.SetGraphicsOut(GxOutSetting.Contour);
+        SetTemp2m();
+        engine.GradsCommandInterface.Display("t2m");
+
+        var outputFile = Path.GetTempFileName();
+        
+        engine.GradsCommandInterface.printim(outputFile, horizontalSize:1024, verticalSize:768, format: OutputFormat.PNG);
+        
+        var generated = Bitmap.FromFile(outputFile) as Bitmap;
+        var expected = Bitmap.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("GradsSharp.UnitTests.Data.Expected.contour-test.png")) as Bitmap;
+        
+        // compare the bitmaps and assert they are equal
+        Helpers.CompareBitmaps(generated, expected);
+        
+        generated.Dispose();
+        expected.Dispose();
+        
+        File.Delete(outputFile);
+        
+
+        
+    }
+
+
+    private void SetTemp2m()
+    {
+        IGradsGrid data = engine.GradsCommandInterface.GetVariable(new VariableDefinition()
+        {
+            HeightType = FixedSurfaceType.SpecifiedHeightLevelAboveGround,
+            HeightValue = 2,
+            VariableType = DataVariable.Temperature
+        });
+
+        data = data.Subtract(273.15);
+
+        engine.GradsCommandInterface.Define("t2m", data);
+    }
+}
---
+++ test/GradsSharp.UnitTests/Helpers.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using Shouldly;
+
+namespace GradsSharp.UnitTests;
+
+public class Helpers
+{
+    public static void CompareBitmaps(Bitmap bitmap1, Bitmap bitmap2)
+    {
+        bitmap1.Width.ShouldBe(bitmap2.Width);
+        bitmap1.Height.ShouldBe(bitmap2.Height);
+
+        for (int y = 0; y < bitmap1.Height; y++)
+        {
+            for (int x = 0; x < bitmap1.Width; x++)
+            {
+                bitmap1.GetPixel(x, y).ShouldBe(bitmap2.GetPixel(x, y));
+            }
+        }
+    }
+}
